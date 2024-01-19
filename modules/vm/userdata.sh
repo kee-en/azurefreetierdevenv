d5fe6046feff9e422af596857bba61933896aedd
Kien Dolorfo <kiendolorfo@gmail.com>
2024-01-19 17:46:22 +0800

    Add userdata and Fix Terraform Code

---
+++ modules/vm/userdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo sed -i 's/#Port 22/Port 2020/' /etc/ssh/sshd_config
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
