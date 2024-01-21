1f8d42b5ab11214b0870613fd028856f84088810
Kien Dolorfo <kiendolorfo@gmail.com>
2024-01-21 12:41:55 +0800

    fix budget/main.tf

--- modules/budget/main.tf
+++ modules/budget/main.tf
@@ -5,7 +5,8 @@ resource "azurerm_consumption_budget_resource_group" "dev_budget" {
   time_grain = "Monthly"
 
   time_period {
-    start_date = "024-01-01T00:00:00Z"
+    start_date = "2024-01-01T00:00:00Z"
+    end_date = "2025-01-01T00:00:00Z"
   }
 
   notification {
